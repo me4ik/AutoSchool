94d8c63973fab29d96a220de209bd7d9099b7b63
KaelTheVoker <mrfoxy3099@gmail.com>
2023-05-21 15:14:40 +0300

    Логика машины
    Добавил переключение скоростей при пересечении триггеров, создал три триггера с разными тегами

---
+++ Assets/CarLogic.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.ShaderGraph.Internal;
+using UnityEngine;
+
+public class CarLogic : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    public float speed = 1500f;
+    private float slowSpeed;
+    private float standartSpeed;   //переменные скоростей
+    private float fastSpeed;
+
+    private bool Standart = true;
+    private bool Speeding = false;  //логические переменные для изменения скорости
+    private bool Slowing = false;
+
+    private float ChangeRate = 2f; //Скорость изменения скорости машины (чем больше тем быстрее)
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        slowSpeed = speed / 1.3f;
+        fastSpeed = speed * 1.3f;  //определение медленной, быстрой и стандартных скоростей
+        standartSpeed = speed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        rb.velocity = speed * Time.deltaTime * transform.forward;
+
+        if (Standart)
+        {
+            StandartSpeed();
+        }
+
+        if (Speeding)
+        {
+            FastSpeed();               // изменение скорости в зависимости от значений логических переменных
+        }
+
+        if (Slowing)
+        {
+            SlowSpeed();
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Slow"))
+        {
+            Slowing = true;
+            Standart = false;
+            Speeding = false;
+        }
+
+        if (other.CompareTag("Fast"))
+        {
+            Slowing = false;
+            Standart = false;               // изменение логических переменных при заходе в разные триггеры
+            Speeding = true;
+        }
+
+        if (other.CompareTag("Standart"))
+        {
+            Slowing = false;
+            Standart = true;
+            Speeding = false;
+        }
+    }
+
+
+    public void SlowSpeed()
+    {
+        speed = Mathf.Lerp(speed, slowSpeed, ChangeRate * Time.deltaTime);
+    }
+
+    public void StandartSpeed()
+    {
+        speed = Mathf.Lerp(speed, standartSpeed, ChangeRate * Time.deltaTime);      //методы для плавного изменения скоростей
+    }
+
+    public void FastSpeed()
+    {
+        speed = Mathf.Lerp(speed, fastSpeed, ChangeRate * Time.deltaTime);
+    }
+
+}
